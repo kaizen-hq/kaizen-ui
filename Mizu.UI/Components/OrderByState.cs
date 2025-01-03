44ce1e65d16696ccd91e88288decd40d3d034459
Zach Burke <zach.r.burke@gmail.com>
2025-01-03 17:58:48 -0600

    readonly grid, panel

---
+++ Mizu.UI/Components/OrderByState.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+
+namespace Mizu.UI.Components
+{
+    public record OrderBy<TSource, TKey>(string Name, bool Descending, Expression<Func<TSource, TKey>> SortFunc);
+
+    public record OrderByState<TSource, TKey>
+    {
+        public List<OrderBy<TSource, TKey>> OrderBy { get; set; } = new();
+    
+        [NotNull]
+        public Action<OrderBy<TSource, TKey>, bool>? UpdateOrderBy { get; set; }
+    }
+}
---
+++ Mizu.Web/Program.cs
@@ -0,0 +1,34 @@
+using Mizu.Web.Client.Pages;
+using Mizu.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Mizu.Web.Client._Imports).Assembly);
+
+app.Run();
