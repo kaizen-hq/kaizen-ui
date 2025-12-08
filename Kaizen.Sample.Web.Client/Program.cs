cb5a0ef88823e40e38cc805e9fa87d73741cb6ab
Zach Burke <zach.burke@kaizen.io>
2025-12-08 12:18:54 -0600

    Making the whole project WASM only

--- Kaizen.Sample.Web.Client/Program.cs
+++ Kaizen.Sample.Web.Client/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Kaizen.UI;
+using Kaizen.Sample.Web.Client;
 using Kaizen.Sample.Web.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<Routes>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
 builder.Services.AddSingleton<ToastService>();
 builder.Services.AddSingleton<SampleCodeService>();
 
